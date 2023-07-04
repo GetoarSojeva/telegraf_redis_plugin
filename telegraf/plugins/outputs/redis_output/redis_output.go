316f9936a935ee6e627c42e7eadcd14bdfdb734f
G S <sojeva@yahoo.de>
2023-07-04 14:49:00 +0200

    edit redis plugin

--- telegraf/plugins/outputs/redis_output/redis_output.go
+++ telegraf/plugins/outputs/redis_output/redis_output.go
@@ -37,22 +37,66 @@ func (r *RedisOutput) Description() string {
 
 func (r *RedisOutput) Write(metrics []telegraf.Metric) error {
 	conn, err := redis.Dial("tcp", r.Servers[0])
+
 	if err != nil {
 		return fmt.Errorf("unable to connect to Redis server: %v", err)
 	}
+
 	defer conn.Close()
 
 	for _, metric := range metrics {
-		value := metric.Fields()["value"].(string)
-		// fmt.Println(value)
 
-		_, err = conn.Do("RPUSH", r.Queue, value)
-		if err != nil {
-			return fmt.Errorf("error pushing metric to Redis: %v", err)
+		// metrics from input.redis_input plugin
+		measurement := metric.Name()
+		if measurement == "redis_queue" {
+			value := metric.Fields()["value"].(string)
+
+			fmt.Println(value)
+
+			_, err = conn.Do("RPUSH", r.Queue, value)
+
+			if err != nil {
+				return fmt.Errorf("error pushing metric to Redis: %v", err)
+			}
+
+		} else {
+			value := generateMetricString(metric)
+
+			_, err = conn.Do("RPUSH", r.Queue, value)
+
+			if err != nil {
+				return fmt.Errorf("error pushing metric to Redis: %v", err)
+			}
+
 		}
 	}
-
 	return nil
+}
+
+// Function to generate the string representation of the metric
+func generateMetricString(metric telegraf.Metric) string {
+	measurement := metric.Name()
+	tags := metric.Tags()
+	fields := metric.Fields()
+	time := metric.Time().UnixNano()
+
+	// Construct the string representation
+	str := fmt.Sprintf("%s", measurement)
+
+	// Add tags
+	for key, value := range tags {
+		str += fmt.Sprintf(",%s=%s", key, value)
+	}
+
+	// Add fields
+	for key, value := range fields {
+		str += fmt.Sprintf(" %s=%v", key, value)
+	}
+
+	// Add time
+	str += fmt.Sprintf(" %d", time)
+
+	return str
 }
 
 func init() {
